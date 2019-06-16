888dd5fad7c8846de50b8896a9012a80d1b01e68
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-06-16 19:05:38 +0900

    support args

--- calc_NMSE.py
+++ calc_NMSE.py
@@ -2,6 +2,13 @@ import numpy as np
 import math
 import cv2
 
+# Check arguments
+import sys
+args = sys.argv
+if len(args) != 3:
+    print("\nUSAGE : $ python calc_NMSE.py [reference_image] [evaluation_image]")
+    sys.exit()
+
 def read_img(_img_name):
 	# read input image
 	img = cv2.imread(_img_name)
@@ -10,65 +17,6 @@ def read_img(_img_name):
 	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 	return img
-
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL1.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR1.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL2.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR2.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL3.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR3.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL4.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR4.bmp")
-img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL5.bmp")
-img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR5.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL6.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR6.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL7.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR7.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL8.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR8.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL9.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR9.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL10.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR10.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL20.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR20.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL30.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR30.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL40.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR40.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL50.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR50.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL60.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR60.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL70.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR70.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL80.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR80.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL90.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR90.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL100.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR100.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL110.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR110.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL120.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR120.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL130.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR130.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL140.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR140.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL150.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR150.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL160.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR160.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL170.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR170.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL180.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR180.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL190.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR190.bmp")
-# img_I_RGB = read_img("images/DATA/uniformly_new/1024/plane_10M_RL200.bmp")
-# img_K_RGB = read_img("images/DATA/uniformly_new/1024_1e-05/gaussian_plane_LR200.bmp")
 
 def NMSE(_img_I_RGB, _img_K_RGB):
     # Convert RGB to Gray
@@ -86,4 +34,9 @@ def NMSE(_img_I_RGB, _img_K_RGB):
     return NMSE
 
 if __name__ == "__main__":
-    print("NMSE =", NMSE(img_I_RGB, img_K_RGB))
+    # Read two images
+    img_ref_I_RGB = read_img( args[1] )
+    img_eva_K_RGB = read_img( args[2] )
+
+    # Calc NMSE
+    print("\nNMSE =", NMSE(img_ref_I_RGB, img_eva_K_RGB), "\n")
--- graph_NMSE.py
+++ graph_NMSE.py
@@ -1,7 +1,3 @@
-#
-# NMSE
-#
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,30 +10,23 @@ plt.rc('grid', color='w', linestyle='solid')
 plt.rc('patch', edgecolor='#E6E6E6')
 plt.rc('lines', linewidth=2)
 
+# Check arguments
+import sys
+args = sys.argv
+if len(args) != 2:
+    print("\nUSAGE   : $ python graph_NMSE.py [csv_file_path]")
+    sys.exit()
 
 
-# -------------------------
-# ----- Read csv file -----
-# -------------------------
-# original
-LR_NMSE = pd.read_csv("NMSE.csv", header=None)
+# Read csv file
+csv = pd.read_csv(args[1], header=None)
 
-LR_NMSE = LR_NMSE.values
+# Convert to numpy
+LR_NMSE = csv.values
+LR   = LR_NMSE[:,0]
+NMSE = LR_NMSE[:,1]
 
-# 0, 10, 100, 200
-#index = [0, 2, 11, 13]
-
-LR   = LR_NMSE[:16,0]
-NMSE = LR_NMSE[:16,1]
-# std_original = original[:14,2]
-# RL_original  = original[index,0]
-# api_original = original[index,1]
-
-
-
-# ----------------
-# ----- Plot -----
-# ----------------
+# Create figure
 # plt.plot(LR, NMSE, color='black')
 c = plt.scatter(LR, NMSE, color='black')
 
@@ -47,23 +36,10 @@ plt.rcParams["font.size"] = 14
 plt.xlabel('$L$', fontsize=14)
 plt.ylabel('NMSE', fontsize=14) # Gray scale
 
-# draw circle
-# plt.scatter(RL_original[2], api_original[2], facecolor='none', s=200, edgecolor='black')
-# plt.scatter(RL_noised[2], api_noised[2], facecolor='none', s=200, edgecolor='black')
-# plt.scatter(RL_original[11], api_original[11], facecolor='none', s=200, edgecolor='black')
-# plt.scatter(RL_noised[11], api_noised[11], facecolor='none', s=200, edgecolor='black')
-
-# Draw text
-# plt.text(5, 20.5, "20.5", fontsize=14, color='black')
-# plt.text(10, 1.0, "0.336", fontsize=14, color='black')
-# plt.text(100, 0.8, "0.0318", fontsize=14, color='black')
-
 plt.xticks([1, 50, 100, 150], fontsize=14)
-#plt.xticks([0, 10, 100])
-plt.yticks([0, 5, 10, 15, 20], fontsize=14)
-#plt.ylim([0, 275])
+plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], fontsize=14)
 
-#plt.grid()
+plt.grid()
 plt.legend(fontsize=12)
 
 plt.show()
