ab72bf8b4ca731c18c83dd93e44961bc9447369b
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-11-30 13:05:43 +0900

    Update

--- src/calc_entropy.py
+++ src/calc_entropy.py
@@ -7,38 +7,37 @@ args = sys.argv
 if len(args) != 2:
     sys.exit()
 
-def read_image(_img_name):
-    # read input image
-    img_BGR = cv2.imread(_img_name)
-
-    # convert color (BGR → RGB)
-    img_RGB = cv2.cvtColor(img_BGR, cv2.COLOR_BGR2RGB)
+def read_image( _img_name ):
+    img_BGR = cv2.imread( _img_name )
+    img_RGB = cv2.cvtColor( img_BGR, cv2.COLOR_BGR2RGB )
 
     return img_RGB
 
-def calc_entropy(_histogram, _num_of_pixels):
+def calc_entropy( _histogram, _num_of_pixels ):
     entropy = 0
 
-    for i in range(256):
-        p = _histogram[i]/_num_of_pixels
+    for i in range( 256 ):
+        # Calc the probability of appearance of pixel value "i"
+        p = _histogram[i] / _num_of_pixels
+
         if p == 0:
             continue
-        entropy -= p*np.log2(p)
+        entropy -= p * np.log2( p )
 
     return entropy
 
 if __name__ == "__main__":
-    # Read two input images
-    img_in_RGB = read_image(args[1])
+    # Read one image
+    img_in_RGB = read_image( args[1] )
 
     # Convert RGB to Grayscale
-    img_in_Gray = cv2.cvtColor(img_in_RGB, cv2.COLOR_RGB2GRAY)
+    img_in_Gray = cv2.cvtColor( img_in_RGB, cv2.COLOR_RGB2GRAY )
 
     # Get histogram of the input image
-    img_hist, img_bins = np.histogram(np.array(img_in_Gray).flatten(), bins=np.arange(256+1))
-    # print(img_hist.shape) (255,)
+    img_hist, img_bins = np.histogram( np.array( img_in_Gray ).flatten(), bins=np.arange( 256+1 ) )
+    # print( img_hist.shape ) (255,)
 
     # Calculate entropy of the input image
     num_of_pixels = img_in_RGB.shape[0] * img_in_RGB.shape[1]
-    entropy = calc_entropy(img_hist, num_of_pixels)
-    print(f'Entropy: {entropy:.2f}')
+    entropy = calc_entropy( img_hist, num_of_pixels )
+    print( 'Entropy: {:.2f}'.format( entropy ) )
